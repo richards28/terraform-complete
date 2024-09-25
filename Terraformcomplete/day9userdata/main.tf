4b25f524f64c968d350b9846c7e93e47182b0307
richards28 <richirocks96@gmail.com>
2024-09-25 11:27:33 +0530

    Update main.tf

--- Terraformcomplete/day9userdata/main.tf
+++ Terraformcomplete/day9userdata/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "name" {
-    ami = "ami-013a28d7c2ea10269"
+    ami = "ami-0f75d1a8c9141bd00"
     instance_type = "t2.micro"
     key_name = "keypair2"
     #user_data = file("script.sh")
